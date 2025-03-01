818cbec00ffa9ae267a873ce2fc784ed80c3c3ec
Amina Rakhbari <amina.rakhbari@yandex.ru>
2025-03-01 14:02:05 +0300

    реализовала форму со свойствами и ее закрытие/открытие

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,90 @@
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (props: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ articleState }: ArticleParamsFormProps) => {
+	const refElement = useRef<HTMLDivElement | null>(null);
+	const [isSidebarOpen, setSidebarOpen] = useState(false);
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontsize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleState.contentWidth);
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+		<div ref={refElement}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => setSidebarOpen(!isSidebarOpen)}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isSidebarOpen,
+				})}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={setFontFamily}
+					/>
+					<RadioGroup
+						selected={fontSize}
+						options={fontSizeOptions}
+						name='fontSize'
+						title='Размер шрифта'
+						onChange={setFontsize}
+					/>
+					<Select
+						selected={fontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+						onChange={setFontColor}
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+						onChange={setBackgroundColor}
+					/>
+					<Select
+						selected={contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={setContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
